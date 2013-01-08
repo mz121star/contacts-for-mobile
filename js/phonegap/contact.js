2850c750196655c4a761c0dee94510499e766090
mz121star <miaozhuang@0121>
2013-01-08 22:12:21 +0800

    modified

--- js/phonegap/contact.js
+++ js/phonegap/contact.js
@@ -2,16 +2,21 @@
     Pgap = this.Pgap || {};
 
     Pgap.contacts = function () {
+
     }
 
     Pgap.contacts.prototype.find = function (name, scuess, error) {
         // 从所有联系人中查找任意名字字段中包含“Bob”的联系人
-        var onSuccess=scuess||{},onError=error||{};
-        var options = new ContactFindOptions();
-        options.filter = name;
-        var fields = ["displayName", "name"];
-        navigator.contacts.find(fields, onSuccess, onError, options);
-    }
+
+        document.addEventListener("deviceready", function(){
+            var onSuccess=scuess||{},onError=error||{};
+            var options = new ContactFindOptions();
+            options.filter = name;
+            var fields = ["displayName", "name"];
+            navigator.contacts.find(fields, onSuccess, onError, options);
+        }, false);
+
+     }
 //    function onSuccess(contacts) {
 //        alert('Found ' + contacts.length + ' contacts.');
 //    }
