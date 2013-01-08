56317cfb0e3e0d5377a26fbc1560ba32f52baf17
mz121star <miaozhuang@0121>
2013-01-08 22:30:19 +0800

    modified

---
+++ js/phonegap/application.js
@@ -0,0 +1,7 @@
+ (function(){
+    var Pgap = this.Pgap || {};
+     Pgap.onLoad=function(onDeviceReady){
+         document.addEventListener("deviceready", onDeviceReady, false);
+     }
+     this.Pgap=Pgap;
+ }).call(this)
--- js/phonegap/contact.js
+++ js/phonegap/contact.js
@@ -1,22 +1,20 @@
 (function () {
-    Pgap = this.Pgap || {};
+   var Pgap = this.Pgap || {};
 
     Pgap.contacts = function () {
-
+         console.log("create contacts object success!");
     }
 
     Pgap.contacts.prototype.find = function (name, scuess, error) {
-        // 从所有联系人中查找任意名字字段中包含“Bob”的联系人
-
-        document.addEventListener("deviceready", function(){
+              console.log("find function running!");
             var onSuccess=scuess||{},onError=error||{};
             var options = new ContactFindOptions();
             options.filter = name;
             var fields = ["displayName", "name"];
             navigator.contacts.find(fields, onSuccess, onError, options);
-        }, false);
 
-     }
+
+     };
 //    function onSuccess(contacts) {
 //        alert('Found ' + contacts.length + ' contacts.');
 //    }
