a03080589639e4555ef06f621459af519d584e14
mz121star <mz121star@gmail.com>
2013-01-08 17:54:02 +0800

    phonegap

---
+++ js/phonegap/contact.js
@@ -0,0 +1,23 @@
+(function (window,Pgap) {
+    Pgap = Pgap || {};
+
+    Pgap.contacts = function () {
+    }
+
+    Pgap.contacts.prototype.find = function (name, scuess, error) {
+        // 从所有联系人中查找任意名字字段中包含“Bob”的联系人
+        var onSuccess=scuess||{},onError=error||{};
+        var options = new ContactFindOptions();
+        options.filter = name;
+        var fields = ["displayName", "name"];
+        navigator.service.contacts.find(fields, onSuccess, onError, options);
+    }
+//    function onSuccess(contacts) {
+//        alert('Found ' + contacts.length + ' contacts.');
+//    }
+//
+//    function onError() {
+//        alert('onError!');
+//    }
+     window.Pgap=Pgap;
+})(window,Pgap);
